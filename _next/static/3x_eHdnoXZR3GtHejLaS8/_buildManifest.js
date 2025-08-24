6f30eb2b1576a286daf1ca47802cd778b5b005e1
christiancrisologo <christiancrisologo@users.noreply.github.com>
2025-08-24 00:04:40 +0000

    deploy: d06a654d0d98d46797048d5bea73e57d3c41dc77

---
+++ _next/static/3x_eHdnoXZR3GtHejLaS8/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/c5a665aad6d3c9b4.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/kids-math-quiz//_next/:path+",
+        "destination": "/kids-math-quiz/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/c5a665aad6d3c9b4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3500f5a01029ffd7.js",
+  "static/chunks/turbopack-f3e23f34b24510c1.js"
+])
